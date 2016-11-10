47c8340f0a3f14e6a5d93817945a7191b194f64b
Daniel Herzog <danfooo@spotify.com>
2016-11-10 15:48:28 +0000

    Add dangling comma to fix linting

--- server/models/todo.js
+++ server/models/todo.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const todoSchema = new Schema({
   text: { type: 'String', required: true },
   completed: { type: 'Boolean', required: true },
-  cuid: { type: 'String', required: true }
+  cuid: { type: 'String', required: true },
 });
 
 export default mongoose.model('Todo', todoSchema);
